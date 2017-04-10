d21613cc7b6cb849ae3889b4175ac83cae38f2a7
Mingqian Ye <mingqianye@Mingqians-MacBook-Pro.local>
2017-04-09 20:40:53 -0700

    work

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [yogthos/config "0.8"]
                  [ring "1.4.0"]
                  [reanimated "0.5.0"]
-                 [solitaire-core "0.1.24"]]
+                 [solitaire-core "0.1.25"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-externs "0.1.6"]
