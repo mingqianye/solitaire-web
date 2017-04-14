658235902a7dc1a005ee6fb309a9594d2bef585f
Mingqian Ye <mingqian.ye@33across.com>
2017-04-14 00:00:14 -0700

    work

---
+++ src/cljs/solitaire_web/solitaire_panel/coordinate_config.cljs
@@ -0,0 +1,21 @@
+(ns solitaire-web.solitaire-panel.coordinate-config)
+
+(def coordinate-config
+  {:1st-card-in-f1   {:x (* 130 3) :y 0  }
+   :1st-card-in-f2   {:x (* 130 4) :y 0  }
+   :1st-card-in-f3   {:x (* 130 5) :y 0  }
+   :1st-card-in-f4   {:x (* 130 7) :y 0  }
+   :1st-card-in-td1  {:x (* 130 0) :y 150}
+   :1st-card-in-td2  {:x (* 130 1) :y 150}
+   :1st-card-in-td3  {:x (* 130 2) :y 150}
+   :1st-card-in-td4  {:x (* 130 3) :y 150}
+   :1st-card-in-td5  {:x (* 130 4) :y 150}
+   :1st-card-in-td6  {:x (* 130 5) :y 150}
+   :1st-card-in-td7  {:x (* 130 6) :y 150}
+
+   :1st-card-in-s    {:x 0         :y 0  }
+   :1st-card-in-w    {:x 200       :y 0  }
+
+   :card-in-s-offset {:x 0.4       :y 0.4}
+   :card-in-w-offset {:x 20        :y 0  }
+   })
