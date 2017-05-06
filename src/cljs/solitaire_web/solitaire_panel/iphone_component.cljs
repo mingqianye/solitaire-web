324156225a6ad07272fbf946826bec85dc51ea15
Mingqian Ye <mingqianye@Mingqians-MacBook-Pro.local>
2017-05-05 17:17:24 -0700

    work

--- src/cljs/solitaire_web/solitaire_panel/iphone_component.cljs
+++ src/cljs/solitaire_web/solitaire_panel/iphone_component.cljs
@@ -2,11 +2,22 @@
   (:require 
     [re-frame.core :refer [subscribe]]))
 
-(defn iphone-component []
+(defn iphone-screen []
   (let [balance (subscribe [:balance])]
     (fn []
-    [:div
-     {:style {:position "absolute" :left 0 :bottom 0}}
-     [:i {:class "zmdi zmdi-balance zmdi-hc-5x"}]
-     (str "$ " @balance)
-     ])))
+      [:div
+        [:i {:class "zmdi zmdi-balance zmdi-hc-5x"}]
+        (str "$ " @balance)
+       ]
+      )))
+
+
+
+(defn iphone-component []
+  [:div
+   {:style {:width "18vw" :position "absolute" :left 0 :bottom 0 :transform "translate3d(20%, 50%, 0)"}}
+   [:img {:src "images/iphone.png" 
+          :filter "drop-shadow(9px 9px 9px rgba(0,0,0,0.3))"
+          :width "100%"}]
+   [iphone-screen]
+   ])
